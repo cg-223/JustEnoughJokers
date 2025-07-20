ffd1d978fe228e2c9e5dcf952e192c98e3050e83
cg223 <cg223celeste@gmail.com>
2025-07-20 16:08:08 -0400

    dont apply pinned sticker on pinned cards
    this causes a bug where if you C in a pinned card with DebugPlus it stays
     pinned in your jokers, whyd nobody tell me about this

--- localization/en-us.lua
+++ localization/en-us.lua
@@ -8,6 +8,15 @@ return {
                     "each {C:attention}Joker{} in your {C:attention}Collection{}",
                     "{C:inactive}(Currently {C:chips}+#2#{C:inactive} Chips)"
                 }
+            },
+            j_jej_cleanfreak = {
+                name = "Clean Freak",
+                text = {
+                    "If all held {C:attention}Jokers{} are sorted",
+                    "by {C:red}Rarity{}, gives {X:mult, C:white}X#1#{} Mult",
+                    "{C:inactive}in ascending order",
+                    "{C:inactive}Modded rarities may be in any location"
+                }
             }
         }
     },
